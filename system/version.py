2f2932dbda78216222ee1b2cbceef142a016be5a
Joshua Auger <joshua@Joshuas-MacBook-Air.local>
2026-05-28 02:11:55 -0400

    version: add joshuaauger/openpilot to sunnypilot_remote list

--- system/version.py
+++ system/version.py
@@ -106,7 +106,8 @@ class OpenpilotMetadata:
     return self.git_normalized_origin in ("github.com/sunnypilot/sunnypilot",
                                           "github.com/sunnypilot/openpilot",
                                           "github.com/sunnyhaibin/sunnypilot",
-                                          "github.com/sunnyhaibin/openpilot")
+                                          "github.com/sunnyhaibin/openpilot",
+                                          "github.com/joshuaauger/openpilot")
 
   @property
   def git_normalized_origin(self) -> str:
